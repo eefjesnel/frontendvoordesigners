9628e58ed2f902ff6f8e4795512dfd7797deaa13
eefjesnel <112808218+eefjesnel@users.noreply.github.com>
2023-06-17 16:33:11 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,29 @@
 // JavaScript Document
 console.log("howdy");
+
+// library --> https://sortablejs.github.io/Sortable/
+// documentatie --> 
+
+var kledingkast = document.querySelector("section:nth-of-type(1) ul:first-of-type");
+var outfit = document.querySelector("section:nth-of-type(2) ul:last-of-type");
+
+
+new Sortable(kledingkast, {
+	group: {
+		name: 'kleding', // zelfde naam als bij allesLijstje
+		pull: 'clone',
+	},
+  sort: false,
+	animation: 300 // een kleine animatie als een foto van plek wisselt
+});
+
+new Sortable(outfit, {
+	group: {
+		name: 'kleding', // zelfde naam als bij favoLijstje 
+    pull: 'true'
+	},
+	animation: 300 // een kleine animatie als een foto gecloned wordt
+});
+
+
+
